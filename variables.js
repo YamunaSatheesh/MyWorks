1655a11c5eb042565be3503ca4603f8c68a5b7b9
YamunaSatheesh <63958657+YamunaSatheesh@users.noreply.github.com>
2025-08-16 18:56:12 +0530

    day 1 variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+var firstname="Yamuna"
+console.log(firstname)
+console.log(typeof firstname)
+var companyName= "TCS"
+console.log(companyName)
+console.log(typeof companyName)
+var mobileNumber =9789542890
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+var isAutomation = true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+var hasPlaywright 
+console.log(hasPlaywright)
